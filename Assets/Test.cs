0fd4a6f496ce16b9281c167a5e7fc652d1d6bea6
lisa-nakanishi <iwarehiko3@gmail.com>
2021-04-22 16:44:04 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    //use this for initialization
+    void Start()
+    {
+        //配列を初期化する
+        int[] points = { 1, 2, 3, 4, 5 };
+
+        //配列の要素を表示する
+        for (int i = 0; i < 5; i++)
+        
+        {
+          Debug.Log(points[i]);
+        }
+
+        
+
+
+         //配列の要素を逆順にする
+         for (int a = 4; a > -1; a--)
+
+         {
+           Debug.Log(points[a]);
+         }
+
+
+    }
+    
+
+    //update is called once per frame void update()
+    void Update()
+    {
+        
+    }
+}
+
